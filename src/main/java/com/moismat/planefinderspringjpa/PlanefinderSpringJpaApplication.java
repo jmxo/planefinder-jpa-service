3d3a30d85b233d5cb6356ca8e255d939c0c97126
mo.ismat <mo.ismat@auto1.com>
2022-04-24 18:26:54 +0200

    init commit

---
+++ src/main/java/com/moismat/planefinderspringjpa/PlanefinderSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.moismat.planefinderspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanefinderSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanefinderSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moismat/planefinderspringjpa/PlanefinderSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moismat.planefinderspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanefinderSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
