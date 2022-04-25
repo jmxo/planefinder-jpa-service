2582739193a52a2934ad67b740cfb0940b2dea1f
mo.ismat <mo.ismat@auto1.com>
2022-04-25 18:28:08 +0200

    create and populate table with sql script

--- src/main/java/com/moismat/planefinderspringjpa/PlaneFinderPoller.java
+++ src/main/java/com/moismat/planefinderspringjpa/PlaneFinderPoller.java
@@ -21,15 +21,6 @@ class PlaneFinderPoller {
     @Scheduled(fixedRate = 1000)
     private void pollPlanes() {
 
-        repository.deleteAll();
-
-        client.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(repository::save);
-
         repository.findAll().forEach(System.out::println);
 
     }
